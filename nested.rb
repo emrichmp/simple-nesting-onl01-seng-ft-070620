ae24eb61c08702f76b101fa1b74d2d2ab5af5f3a
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-12 22:07:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     value = programmer_hash[:dennis_ritchie][:languages]
+     value = programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
