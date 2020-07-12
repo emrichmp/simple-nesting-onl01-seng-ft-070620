f7ad84a192c41b1f159967225b5231b26889230d
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-12 22:14:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    programmer_hash[:yukihiro_matsumoto] => {:known_for => "Ruby", :languages => ["LISP", "C"]}}
+     
+    programmer_hash[:yukihiro_matsumoto] => {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+    
+    #:known_for => "Ruby", :languages => ["LISP", "C"]}
     
 end
 
